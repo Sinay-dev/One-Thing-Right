273fc45bcbc0cce1ea7eea91ee9b3d17533d76b2
Sinay-dev <62427105+Sinay-dev@users.noreply.github.com>
2020-04-05 15:20:05 +0200

    Update otrflask.py

--- otrflask.py
+++ otrflask.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = '5aa97e5e0fb05044543228ad1d898ae9'
 
 
 @app.route("/")
@@ -7,17 +11,40 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+
 @app.route("/liste")
 def liste():
     return render_template('liste.html', title="Liste")
+
 
 @app.route("/inv")
 def inventaire():
     return render_template('inventaire.html')
 
+
 @app.route("/shop")
 def shop():
     return render_template('shop.html')
+    
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title=Inscription, form=form )
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Vous êtes connecté', 'success')
+        return redirect(url_for('home'))
+    return render_template('login.html', title=Login, form=form )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
